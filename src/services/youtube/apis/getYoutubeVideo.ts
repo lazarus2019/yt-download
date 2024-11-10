8aaa28ec2f2991ffa6bc6b647eb788798efcaf12
Thai Son <thaisonbk2020@gmail.com>
2024-11-10 14:58:36 +0700

    fix(service): update response via axios

--- src/services/youtube/apis/getYoutubeVideo.ts
+++ src/services/youtube/apis/getYoutubeVideo.ts
@@ -1,16 +1,15 @@
 import { axiosGGYoutubeQuery } from '@/apis/axios';
-import { VideoQueryParams, VideoQueryRes } from '../types';
-import { Response } from '@/types/common';
 import { youtubeQueryKeys } from '../queryKeys';
+import { VideoQueryParams, VideoQueryRes } from '../types';
 
 const getGGYoutubeVideo = async ({
   q,
   part = 'snippet',
-  maxResults = 10,
+  maxResults = 48, // maximum is 50
   regionCode = 'VN',
   type = 'video',
 }: VideoQueryParams) => {
-  const data = await axiosGGYoutubeQuery.get<Response<VideoQueryRes>>('', {
+  const data = await axiosGGYoutubeQuery.get<VideoQueryRes>('', {
     params: {
       q,
       part,

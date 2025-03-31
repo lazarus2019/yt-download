e4d94510c951af8472c3b1a2b9f94e53769aa15d
lazarus2019 <thaisonbk2019@gmail.com>
2025-03-31 07:52:10 +0700

    fix(SearchInput): handle submit value

--- src/components/search/SearchInput.tsx
+++ src/components/search/SearchInput.tsx
@@ -1,10 +1,10 @@
 import { KeyboardEvent, useState } from 'react';
 
-import classNames from './search.module.scss';
-import { useQuery } from '@tanstack/react-query';
-import { getYoutubeSuggestionQuery } from '@/services/youtube/apis/getYoutubeSuggestion';
-import clsx from 'clsx';
 import { useDebounce } from '@/hooks/useDebounce';
+import { getYoutubeSuggestionQuery } from '@/services/youtube/apis/getYoutubeSuggestion';
+import { useQuery } from '@tanstack/react-query';
+import clsx from 'clsx';
+import classNames from './search.module.scss';
 
 interface SearchInputProps {
   defaultValue?: string;
@@ -18,7 +18,7 @@ export const SearchInput = ({
   placeholder = 'Enter value',
 }: SearchInputProps) => {
   const [value, setValue] = useState(defaultValue);
-  const submitValue = useDebounce(value);
+  const submitValue = useDebounce(value, 500);
   const [selectedIndex, setSelectedIndex] = useState(0);
 
   const [enableSubmit, setEnableSubmit] = useState(false);
@@ -35,15 +35,26 @@ export const SearchInput = ({
   };
 
   const handleEnter = (event: KeyboardEvent) => {
-    if (event.key === 'Enter') {
-      handleSubmit();
-      setEnableSubmit(true);
+    if (event.key !== 'Enter') return;
+
+    if (selectedIndex === -1) {
+      if (!submitValue || submitValue.trim() === '') return;
+
+      onSubmit(submitValue);
+    } else {
+      if (!suggestionData || suggestionData.length === 0) return;
+
+      setValue(suggestionData[selectedIndex]);
+      setEnableSubmit(false);
+      setSelectedIndex(-1);
+      onSubmit(suggestionData[selectedIndex]);
     }
   };
 
   const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setValue(event.target.value);
     setEnableSubmit(true);
+    setSelectedIndex(-1);
   };
 
   const handleKeyDown = (event: KeyboardEvent) => {
@@ -58,6 +69,13 @@ export const SearchInput = ({
           (suggestionData?.length ?? 0)
       );
     }
+    // else if (event.key === 'Enter') {
+    //   if (!suggestionData || suggestionData.length === 0) return;
+
+    //   setValue(suggestionData[selectedIndex]);
+    //   setEnableSubmit(false);
+    //   setSelectedIndex(-1);
+    // }
   };
 
   return (
@@ -77,21 +95,23 @@ export const SearchInput = ({
       </button>
 
       <div className={classNames['suggestion-list']}>
-        {suggestionData &&
-          suggestionData.map((suggestion, index) => (
-            <button
-              key={`suggestion-item-${index}`}
-              className={clsx(classNames['suggestion-item'], {
-                [classNames['active']]: index === selectedIndex,
-              })}
-              onClick={() => {
-                setValue(suggestion);
-                setEnableSubmit(false);
-              }}
-            >
-              {suggestion}
-            </button>
-          ))}
+        {suggestionData
+          ? suggestionData.map((suggestion, index) => (
+              <button
+                key={`suggestion-item-${index}`}
+                className={clsx(classNames['suggestion-item'], {
+                  [classNames['active']]: index === selectedIndex,
+                })}
+                onClick={() => {
+                  setValue(suggestion);
+                  setEnableSubmit(false);
+                  onSubmit(suggestion);
+                }}
+              >
+                {suggestion}
+              </button>
+            ))
+          : null}
       </div>
     </div>
   );

ef0f38d9a58d2499c5774b472b357ccd3bee7ea7
lazarus2019 <thaisonbk2019@gmail.com>
2025-03-31 08:12:25 +0700

    feat(ci): add ci file with bun

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
+import path from 'node:path';
 
 // https://vite.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
-      '@': resolve(__dirname, './src'),
+      '@': path.resolve(__dirname, './src'),
     },
   },
   plugins: [react()],
